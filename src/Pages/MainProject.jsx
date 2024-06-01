a101b7c5a8f2b20f4a1b7960a2303be444a77125
vargha1 <vargha.rahavi@gmail.com>
2024-06-01 15:31:55 +0330

    VR15

--- src/Pages/MainProject.jsx
+++ src/Pages/MainProject.jsx
@@ -1,9 +1,12 @@
+import CaseStudySection from "../components/ProjectComponents/CaseStudySection";
 import Layout from "./Layout";
 import "/src/index.css";
 
 export default function Main() {
 
     return (
-        <Layout bannerTitle={"Passion-Fueled Innovation: Where Dreams Become Reality"} bannerBody={"TekRevol is not just a service provider; we are an invaluable extension of your marketing team, bringing a powerful combination of strategic thinking, expertise, and flawless execution to the areas where you need it the most."} bannerItemContent1={"Strategic Planning"} bannerItemContent2={"Cutting-Edge Technologies"} bannerItemContent3={"Unconventional App Experience"} bannerItemContent4={"Creative Solutions"} />
+        <Layout bannerTitle={"Passion-Fueled Innovation: Where Dreams Become Reality"} bannerBody={"TekRevol is not just a service provider; we are an invaluable extension of your marketing team, bringing a powerful combination of strategic thinking, expertise, and flawless execution to the areas where you need it the most."} bannerItemContent1={"Strategic Planning"} bannerItemContent2={"Cutting-Edge Technologies"} bannerItemContent3={"Unconventional App Experience"} bannerItemContent4={"Creative Solutions"} >
+            <CaseStudySection />
+        </Layout>
     );
 }
--- src/Pages/MainTech.jsx
+++ src/Pages/MainTech.jsx
@@ -1,8 +1,11 @@
 import "/src/index.css";
 import Layout from "./Layout";
+import TechSection from "../components/TechnologiesComponents/TechSection";
 
 export default function MainTech() {
     return (
-        <Layout bannerTitle={"Passion-Fueled Innovation: Where Dreams Become Reality"} bannerBody={"TekRevol is not just a service provider; we are an invaluable extension of your marketing team, bringing a powerful combination of strategic thinking, expertise, and flawless execution to the areas where you need it the most."} bannerItemContent1={"Emerging technology adoption"} bannerItemContent2={"Cross-platform compatibility"} bannerItemContent3={"Agile development process"} bannerItemContent4={"Scalable infrastructure management"} />
+        <Layout bannerTitle={"Passion-Fueled Innovation: Where Dreams Become Reality"} bannerBody={"TekRevol is not just a service provider; we are an invaluable extension of your marketing team, bringing a powerful combination of strategic thinking, expertise, and flawless execution to the areas where you need it the most."} bannerItemContent1={"Emerging technology adoption"} bannerItemContent2={"Cross-platform compatibility"} bannerItemContent3={"Agile development process"} bannerItemContent4={"Scalable infrastructure management"}>
+            <TechSection />
+        </Layout>
     );
 }
